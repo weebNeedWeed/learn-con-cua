cc8b7fbc1b482a04ef92f82cfff9f8e328f4c7da
weebNeedWeed <rivenmle@gmail.com>
2023-12-13 23:33:51 +0700

    lesson 2

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    let secret : u32 = rand::thread_rng().gen_range(1..=100);
+    println!("Secret num = {secret}");
+
+    loop {
+        println!("Please enter your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num, 
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Less"),
+            Ordering::Greater => println!("Greater"),
+            Ordering::Equal => {
+                println!("Equal!");
+                break;
+            }
+        }
+    }
+
+}
